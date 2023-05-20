7e4919dad3592669027bcb14154d034bf49b6192
Lucas Lima <lucas.souza3146@gmail.com>
2023-05-20 18:25:31 -0300

    Instalando e configurando o Mustache parte 2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,7 +6,7 @@ router.get('/', (req: Request, res: Response) => {
     //pegar os produtos do banco de dados
     //organizar as informações desses produtos
     //envia para o template engine
-    res.send('Olá Mundo');
+    res.render('home');
 });
 
 router.get('/contato', (req: Request, res: Response) => {
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
+import mustache from 'mustache-express';
 import mainRoutes from './routes/index';
-import painelRoutes from './routes/painel';
 
 const server = express();
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../public')));
 
 server.use('/', mainRoutes);
-server.use('/painel', painelRoutes);
 
 server.use((req: Request, res: Response) => {
     res.status(404).send('Página não encontrada');
