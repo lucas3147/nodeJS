b8f70ae20ca50c22e6c3e0903b145ffe4ad13dc3
Lucas Lima <lucas.souza3146@gmail.com>
2023-06-03 16:59:39 -0300

    Entendendo o que é CORS

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,22 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import apiRoutes from './routes/api';
 
 dotenv.config();
 
 const server = express();
 
+server.use(cors({
+    
+}));
+
 server.use(express.static(path.join(__dirname, '../public')));
 
 server.use(express.urlencoded({extended: true}));
+
+server.use('/api', apiRoutes);
 
 server.use((req: Request, res: Response) => {
     res.status(404);
