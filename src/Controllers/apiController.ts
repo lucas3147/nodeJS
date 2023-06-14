09d5b9dcb81c0806960b0e6053100a899bb77e2e
Lucas Lima <lucas.souza3146@gmail.com>
2023-06-14 19:16:02 -0300

    JWT: Gerando o Token 1

--- src/Controllers/apiController.ts
+++ src/Controllers/apiController.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express';
+import JWT from 'jsonwebtoken';
 import { User } from '../models/User';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const ping = (req: Request, res: Response) => {
     res.json({pong: true});
@@ -33,7 +37,13 @@ export const login = async (req: Request, res: Response) => {
         });
 
         if(user) {
-            res.json({ status: true });
+            const token = JWT.sign(
+                {id: user.id, email: user.email},
+                process.env.JWT_SECRET_KEY as string,
+                 { expiresIn: '2h'}
+            );
+
+            res.json({ status: true, token });
             return;
         }
     }
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+
 import { User } from '../models/User';
 
 export const Auth = {
