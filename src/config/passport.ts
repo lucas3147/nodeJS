2c481d9d24f924303865916e57b364476d9e4d74
Lucas Lima <lucas.souza3146@gmail.com>
2023-06-17 16:03:28 -0300

    Fazendo JWT com Passport 1

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,28 +1,29 @@
 import passport from 'passport';
-import { BasicStrategy } from 'passport-http';
-import { User, UserInstance } from '../models/User';
-import { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+import {Strategy as JWTStrategy, ExtractJwt} from 'passport-jwt'
+import {User, UserInstance} from '../models/User';
 
-const notAuthorizedJson = { status: 401, message: 'Não autorizado' };
+dotenv.config();
 
-passport.use(new BasicStrategy(async (email, password, done) => {
-    if (email && password) {
-        const user = await User.findOne({
-            where: {email, password}
-        });
-        if(user) {
-            return done(null, user);
-        }
-    }
-
-    return done(notAuthorizedJson, false);
-}));
-
-export const privateRoute = (req: Request, res: Response, next: NextFunction) => {
-    passport.authenticate('basic', (err: Error, user: UserInstance) => {
-        req.user = user;
-        return user ? next() : next(notAuthorizedJson);
-    })(req, res, next);
+const notAuthorizedJson = {
+    status: 401,
+    message: 'Não autorizado'
 }
+
+const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET as string
+}
+
+passport.use(new JWTStrategy(options, async (payload, done) => {
+    
+    const user = await User.findByPk(payload.id)
+
+    if (user) {
+        return done(null, user);
+    } else {
+        return done(notAuthorizedJson, false);
+    }
+}));
 
 export default passport;
