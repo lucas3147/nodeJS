fc8a3638c9c8479eb14b36ac925c3a72e2ee9fcc
Lucas Lima <lucas.souza3146@gmail.com>
2023-06-18 12:52:27 -0300

    Tipos de Matches

--- src/libs/Math.test.ts
+++ src/libs/Math.test.ts
@@ -25,6 +25,44 @@ describe('Testing Math library', () => {
         expect(response).toBe(3);
 
         const response2 = Math.div(3, 0);
-        expect(response2).toBe(false);
+        expect(response2).toBeFalsy();
+    });
+
+    it.only('Contar quantos caracteres tem na string', () => {
+        const response = 'DENIED';
+        expect(response).toHaveLength(6);
+    });
+
+    it.only('Se possui a propriedade e-mail', () => {
+        const response = {
+            name: 'Lucas',
+            email: 'suporte@b7web.com.br'
+        }
+        expect(response).toHaveProperty('email');
+    });
+
+    it.only('Se possui é undefined', () => {
+        const response = undefined;
+        expect(response).toBeUndefined();
+    });
+
+    it.only('Se possuium numero maior que...', () => {
+        const response = 2;
+        expect(response).toBeGreaterThan(15);
+    });
+
+    it.only('Se possuium numero menor que...', () => {
+        const response = 2;
+        expect(response).toBeLessThan(15);
+    });
+
+    it.only('Se retornou uma string que é um email', () => {
+        const response = 'lucas@gmail.com';
+        expect(response).toMatch('[Expressão Regular');
+    });
+
+    it.only('Se retornou um Erro', () => {
+        const response = Math.div(10, 0);
+        expect(response).toThrow(new Error('Não divide por zero'));
     });
 });
