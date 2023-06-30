629fa8c3570735c0252b6cebbff81cfab0664d00
Lucas Lima <lucas.souza3146@gmail.com>
2023-06-29 21:49:57 -0300

    Estabelecendo conexão

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
 server.listen(3000);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('Conexão detectada...');
+
+
+    
+});
