835a7738d6565ec0e091aeee4745c9d646210e00
Lucas Lima <lucas.souza3146@gmail.com>
2023-06-30 21:33:29 -0300

    Entrando no chat 1/2

--- public/main.js
+++ public/main.js
@@ -1,1 +1,20 @@
 const socket = io();
+let username = '';
+let userList = [];
+let loginPage = document.querySelector('#loginPage');
+let chatPage = document.querySelector('#chatPage');
+let loginInput = document.querySelector('#loginNameInput');
+let textInput = document.querySelector('#chatTextInput');
+
+loginPage.style.display = 'flex';
+chatPage.style.display = 'none';
+
+loginInput.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        let name = loginInput.value.trim();
+        if(name != '') {
+            username = name;
+            document.title = 'Chat ('+username+')';
+        }
+    }
+});
