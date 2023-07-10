6b9a9090f1b5aa5422949b3ab10eda6bbb28a781
Lucas Lima <lucas.souza3146@gmail.com>
2023-07-10 13:08:23 -0300

    Planejando a estrutura do banco de dados

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const modelsSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    passwordHash: String,
+    token: String
+});
+
+const modelName = 'User';
+
+if (mongoose.connection && mongoose.connection.models(modelName)) {
+    module.exports = mongoose.connection.models[modelName];
+} else {
+    module.exports = mongoose.model(modelName, modelsSchema)
+}
+
+/*
+
+    Aqui temos a verificação se o model já existe na base de dados:
+
+    Se existir, apenas exportamos ele.
+
+    Se não, criamos esse model do zero.
+
+*/
