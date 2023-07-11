f7b219a200b407855ecba4c0b7a3f2aa74fa1541
Lucas Lima <lucas.souza3146@gmail.com>
2023-07-11 10:32:30 -0300

    Fazendo o cadastro

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,9 @@
+const {mongoose} = require('mongoose');
+const bcrypt = require('bcrypt');
 const { validationResult, matchedData} = require('express-validator');
+
+const User = require('../models/User');
+const State = require('../models/State');
 
 module.exports= {
     signin: async (req, res) => {
@@ -13,6 +18,48 @@ module.exports= {
 
         const data = matchedData(req);
 
-        res.json({tudoCerto: true, data});
+        const user = await User.findOne({
+            email: data.email
+        });
+
+        if(user) {
+            res.json({
+                error: {email: {msg: 'E-mail já existe'}}
+            });
+            return;
+        }
+
+        if (mongoose.Types.ObjectId.isValid(data.state)){
+            const stateItem = await State.findById(data.state);
+
+            if(!stateItem) {
+                res.json({
+                    error: {state: {msg: 'Estado não existe'}}
+                }); 
+                return;
+            }
+        } else {
+            res.json({
+                error: {state: {msg: 'Código de estado inválido'}}
+            }); 
+            return;
+        }
+
+        const passwordHash = await bcrypt.hash(data.password, 10);
+
+        const payload = (Date.now() + Math.random()).toString();
+        const token = await bcrypt.hash(payload, 10);
+
+        const newUser = new User({
+            name: data.name,
+            email: data.email,
+            passwordHash,
+            token,
+            state: data.state
+        })
+
+        await newUser.save();
+
+        res.json({token});
     },
 };
