52746dad3f032c84f7d6282ddbd208acc441dbe8
Lucas Lima <lucas.souza3146@gmail.com>
2023-07-12 14:41:01 -0300

    Exibindo um anúncio 1 de 2

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -151,7 +151,51 @@ module.exports = {
         res.json({ads, total});
     },
     getItem: async (req, res) => {
+        let { id, other = null } = req.query;
 
+        if (!id) {
+            res.json({error: 'Sem produto'});
+            return;
+        }
+
+        if (id.length < 12) {
+            res.json({error: 'ID inválido'});
+            return;
+        }
+        const ad = await Ad.findById(id);
+        if (!ad) {
+            res.json({error: 'Produto inexistente'});
+            return;
+        }
+
+        ad.views++;
+        await ad.save();
+
+        let images = [];
+        for (let i in ad.images) {
+            images.push(`${process.env.BASE}/media/${ad.images[i].url}`);
+        }
+
+        let category = await Category.findById(ad.category).exec();
+        let userInfo = await User.findById(ad.idUser).exec();
+        let stateInfo = await State.findById(ad.state).exec();
+
+        res.json({
+            id: ad._id,
+            title: ad.title,
+            price: ad.price,
+            priceNegotiable: ad.priceNegotiable,
+            description: ad.description,
+            dateCreated: ad.dateCreated,
+            views: ad.views,
+            images,
+            category,
+            userInfo: {
+                name: userInfo.name,
+                email: userInfo.email
+            },
+            stateName: stateInfo.name
+        })
     },
     editAction: async (req, res) => {
 
